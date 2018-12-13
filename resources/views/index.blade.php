fe897b4c86c106ab67086ff5513e8adb8fc9fe27
Jian <jianqilu1126@gmail.com>
2018-12-13 12:55:47 +0000

    save

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,11 +8,11 @@
 			<p class="flow-text"><span class="cyan-text">Date:</span>{{$d->date_add}}</p>
 			<p class="flow-text"><span class="cyan-text">Product:</span>{{$d->product_name}}</p>
 			<p class="flow-text"><span class="cyan-text">Shop:</span>{{$d->product_reference}}</p>
-			<input type="show" value="{{$d->product_name}}" name="product_name">
-			<input type="show" value="{{$d->reference}}" name="order_reference">
-			<input type="show" value="{{$d->id_order}}" name="id_order">
-			<input type="show" value="{{$d->id_customer}}" name="id_customer">
-			<input type="show" value="{{$d->product_id}}" name="product_id">
+			<input type="hidden" value="{{$d->product_name}}" name="product_name">
+			<input type="hidden" value="{{$d->reference}}" name="order_reference">
+			<input type="hidden" value="{{$d->id_order}}" name="id_order">
+			<input type="hidden" value="{{$d->id_customer}}" name="id_customer">
+			<input type="hidden" value="{{$d->product_id}}" name="product_id">
 			<input type="submit" class="btn waves-effect waves-light" value="Accept the Payment">
 			{{csrf_field()}}
 		</form>
