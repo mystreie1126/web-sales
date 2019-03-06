d95b3d338c385a20f58937076269d0faf3e310e2
Jian <jianqilu1126@gmail.com>
2019-03-06 18:08:17 +0000

    save

--- config/app.php
+++ config/app.php
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL',  'public/web-sales'),
+    'url' => env('APP_URL', 'http://localhost/public'),
 
     /*
     |--------------------------------------------------------------------------
