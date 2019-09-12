0c6cd1fc0e455fa4137a75a9886c2541aa4ac71e
Jian <jianqilu1126@gmail.com>
2019-09-12 10:53:38 +0100

    save

--- app/Http/Controllers/preOwnController.php
+++ app/Http/Controllers/preOwnController.php
@@ -7,29 +7,64 @@ use Illuminate\Support\Facades\Auth;
 use DB;
 
 class preOwnController extends Controller
-{
+{	
+
+	private function rockposDeviceStock($category_id){
+		 $stocks = DB::table('c1ft_pos_prestashop.ps_product_shop as a')
+	           			 ->join('c1ft_pos_prestashop.ps_category_product as b','a.id_product','b.id_product')
+	           			 ->join('c1ft_pos_prestashop.ps_product_lang as c','c.id_product','a.id_product')
+	           			 ->join('c1ft_pos_prestashop.ps_product as d','d.id_product','a.id_product')
+	           			 ->join('c1ft_pos_prestashop.ps_stock_available as e','e.id_product','a.id_product')
+	           			 ->select('c.name','d.reference as imei')
+	           			 ->where('a.id_shop',Auth::User()->shop_id)
+	           			 ->where('c.id_shop',Auth::User()->shop_id)
+	           			 ->where('b.id_category',$category_id)
+	           			 ->where('e.quantity',1)
+	           			 ->where('a.active',1)
+	           			 ->get();
+	    return $stocks;
+	}
      public function preownStock(){
         if(Auth::check()){
-	         $stocks =  DB::table('ps_feature_product as a')->select('b.name','a.id_product')
-	            ->join('ps_product_lang as b','a.id_product','b.id_product')
-	            ->join('ps_product_shop as c','c.id_product','a.id_product')
-	            ->join('ps_category_product as d','d.id_product','a.id_product')
-	            ->join('ps_stock_available as e','e.id_product','a.id_product')
-	            ->where('c.id_shop',1)
-	            ->where('c.active',1)
-	            ->where('a.id_feature',2540)
-	            ->where('a.id_feature_value',Auth::User()->feature_value)
-	            ->whereBetween('d.id_category',[1459,1475])
-	            ->where([
-	                ['e.id_shop',0],
-	                ['e.id_shop_group',3],
-	                ['e.id_product_attribute',0],
-	                ['e.quantity','>=',1]
-	            ])
-	           ->groupBy('a.id_product')
-	           ->get();
+	         // $stocks =  DB::table('ps_feature_product as a')->select('b.name','a.id_product')
+	         //    ->join('ps_product_lang as b','a.id_product','b.id_product')
+	         //    ->join('ps_product_shop as c','c.id_product','a.id_product')
+	         //    ->join('ps_category_product as d','d.id_product','a.id_product')
+	         //    ->join('ps_stock_available as e','e.id_product','a.id_product')
+	         //    ->where('c.id_shop',1)
+	         //    ->where('c.active',1)
+	         //    ->where('a.id_feature',2540)
+	         //    ->where('a.id_feature_value',Auth::User()->feature_value)
+	         //    ->whereBetween('d.id_category',[1459,1475])
+	         //    ->where([
+	         //        ['e.id_shop',0],
+	         //        ['e.id_shop_group',3],
+	         //        ['e.id_product_attribute',0],
+	         //        ['e.quantity','>=',1]
+	         //    ])
+	         //   ->groupBy('a.id_product')
+	         //   ->get();
 
-	            return view('preownStock',compact('stocks'));
+
+	           // $preown_stocks = DB::table('c1ft_pos_prestashop.ps_product_shop as a')
+	           // 			 ->join('c1ft_pos_prestashop.ps_category_product as b','a.id_product','b.id_product')
+	           // 			 ->join('c1ft_pos_prestashop.ps_product_lang as c','c.id_product','a.id_product')
+	           // 			 ->join('c1ft_pos_prestashop.ps_product as d','d.id_product','a.id_product')
+	           // 			 ->join('c1ft_pos_prestashop.ps_stock_available as e','e.id_product','a.id_product')
+	           // 			 ->select('c.name','d.reference as imei')
+	           // 			 ->where('a.id_shop',Auth::User()->shop_id)
+	           // 			 ->where('c.id_shop',Auth::User()->shop_id)
+	           // 			 ->where('b.id_category',16)
+	           // 			 ->where('e.quantity',1)
+	           // 			 ->where('a.active',1)
+	           // 			 ->get();
+
+	           // return $preown;
+
+        	$preown_stocks = self::rockposDeviceStock(16);
+        	$brandnew_stocks = self::rockposDeviceStock(17);
+
+	            return view('preownStock',compact('preown_stocks','brandnew_stocks'));
 
         }
     }
--- resources/views/preownStock.blade.php
+++ resources/views/preownStock.blade.php
@@ -5,25 +5,78 @@
 	<input type="hidden" value="{{Auth::User()->feature_value}}">
 @endif
 <div class="container">
-	<button class="btn indigo right" id="preown_export">Export</button>
-
-	<table class="striped" id="preownStock_table">
+	@if(count($preown_stocks) > 0)	
+		<p class="flow-text">Available Pre Own Devices in RockPos</p>
+		<button class="btn indigo right" id="preown_export">Export</button>
 		
-	<thead>
-		<tr>
-			
-		</tr>
-	</thead>
-	<p class="flow-text">Total <span class="orange-text">{{count($stocks)}}</span> devices in stock</p>
+		<table class="striped" id="preownStock_table">	
+			<thead>
+				<tr>
+					<th>Device Name</th>
+					<th>IMEI</th>
+				</tr>
+			</thead>
+			<p class="flow-text">Total <span class="orange-text">{{count($preown_stocks)}}</span> devices</p>
 
-	<tbody>
-		@foreach($stocks as $stock)
-		<tr>
-			<th>{{$stock->name}}</th>
-		</tr>
-		@endforeach
-	</tbody>
-	</table>
+			<tbody>
+				@foreach($preown_stocks as $stock)
+				<tr>
+					<th>{{$stock->name}}</th>
+					<th>{{$stock->imei}}</th>
+				</tr>
+				@endforeach
+			</tbody>
+		</table>
+	@else
+		<p class="flow-text">No Available Pre Own Devices in RockPos</p>
+	@endif
+
+	@if(count($brandnew_stocks) > 0)	
+		<p class="flow-text">Available Brand New Devices in RockPos</p>
+		<button class="btn amber right" id="brandnew_export">Export</button>
+		
+		<table class="striped" id="preownStock_table">	
+			<thead>
+				<tr>
+					<th>Device Name</th>
+					<th>IMEI</th>
+				</tr>
+			</thead>
+			<p class="flow-text">Total <span class="orange-text">{{count($brandnew_stocks)}}</span> devices</p>
+
+			<tbody>
+				@foreach($brandnew_stocks as $stock)
+				<tr>
+					<th>{{$stock->name}}</th>
+					<th>{{$stock->imei}}</th>
+				</tr>
+				@endforeach
+			</tbody>
+		</table>
+	@else
+		<p class="flow-text">No Available Brand New Devices in RockPos</p>
+	@endif
+
+
+	{{-- <p class="flow-text">New Phone Stock</p>
+	<button class="btn indigo right" id="brandnew_export">Export</button>
+	<table class="striped" id="brandNewStock_table">	
+		<thead>
+			<tr>
+				<th>Device Name</th>
+				<th>IMEI</th>
+			</tr>
+		</thead>
+		<p class="flow-text">Total <span class="orange-text">{{count($stocks)}}</span> devices</p>
+
+		<tbody>
+			@foreach($stocks as $stock)
+			<tr>
+				<th>{{$stock->name}}</th>
+			</tr>
+			@endforeach
+		</tbody>
+	</table> --}}
 </div>
 @stop
 
@@ -32,9 +85,17 @@
 	$('#preown_export').click((e)=>{
 		e.preventDefault();
 		$('#preownStock_table').csvExport({
-             title:"Phone Stock"
+             title:"Pre Own Stock"
          });
 	})
+
+	$('#brandnew_export').click((e)=>{
+		e.preventDefault();
+		$('#brandNewStock_table').csvExport({
+             title:"Pre Own Stock"
+         });
+	})
+
 
 
 </script>
